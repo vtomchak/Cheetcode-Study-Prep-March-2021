df737b6d866106a399fe04b0e26885d4be512419
Navya Ramagiri <navyaramagiri@gmail.com>
2021-03-01 19:29:10 -0500

    twoSum

---
+++ Navya/twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+  let memo = {};
+  for (let i = 0; i < nums.length; i++) {
+    let val = target - nums[i];
+    if (nums[i] in memo) {
+      return [i, nums[i]];
+    } else {
+      memo[val] = i;
+    }
+  }
+};
